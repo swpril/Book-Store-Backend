5daa8a8d8aa10137c77755ed696b918e115b9e6c
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-07-29 11:58:33 +0530

    add authorRoute to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { sequelize } from './sequelize';
+import { authors } from './routes/authors';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/authors', authors);
 
 app.listen(PORT, async () => {
 	await sequelize.sync({ force: true });
