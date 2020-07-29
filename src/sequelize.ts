4706d7523dca583a64a7df98f129dbd8c18a1c3c
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-07-29 10:59:35 +0530

    add database

---
+++ src/sequelize.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from 'sequelize-typescript';
+
+export const sequelize = new Sequelize({
+	dialect: 'sqlite',
+	database: 'books',
+	storage: ':memory:',
+	models: [__dirname + '/models'],
+});
