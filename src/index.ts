0eb83fec9d912137d3f99f7b3eaad8d801861b10
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-07-29 12:43:49 +0530

    add bookRoutes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { sequelize } from './sequelize';
 import { authors } from './routes/authors';
+import { books } from './routes/books';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/authors', authors);
+app.use('/books', books);
 
 app.listen(PORT, async () => {
 	await sequelize.sync({ force: true });
---
+++ src/routes/books.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { Book } from '../models/Book';
+
+export const books = Router();
+
+books.post('/', async (req, res) => {
+	try {
+		const book = await Book.create(req.body);
+		res.status(201).send(book);
+	} catch (e) {
+		console.log(e);
+		res.status(400).send();
+	}
+});
+
+books.get('/getAllBooks', async (req, res) => {
+	try {
+		const books = await Book.scope('authors').findAll();
+		res.send(books);
+	} catch (e) {
+		console.log(e);
+		res.status(404).send();
+	}
+});
+
+books.get('/:id', async (req, res) => {
+	try {
+		const book = await Book.scope('authors').findByPk(req.params['id']);
+		res.send(book);
+	} catch (e) {
+		console.log(e);
+		res.status(404).send();
+	}
+});
